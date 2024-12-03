18a5604a343660a425ee92f8dfd40f28c44126a2
Arthur <arttomzak@gmail.com>
2024-12-03 01:31:07 -0600

    working get endpoint

--- src/server.js
+++ src/server.js
@@ -1,34 +1,33 @@
 const express = require("express");
 const cors = require("cors");
-
+const { Pool } = require("pg");
 require("dotenv").config();
-
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to your Express server" });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-// app.get("/api/users", (req,res) => {
-//   try {
+// app.get("/api/users", (req, res) => {
 //     const result = "SELECT * FROM users";
-//     res.json(result.rows);
-//   } catch(err) {
-//     console.error("Query error:", err);
-//     res.status(500).json({ error: "Failed to fetch the users!" });
-//   }
-// });
+//     res.json(result);
+//   }); working but just spits out a string
 
+app.get("/api/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (err) {
+    console.error("Query error:", err);
+    res.status(500).json({ error: "Failed to fetch users" })
+  }
+});
 
 
 const PORT = process.env.PORT || 5432;
