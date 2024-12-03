6fa54b157a54a0607da09e095622a4aa1241a693
Arthur <arttomzak@gmail.com>
2024-12-03 00:49:54 -0600

    fixed broken module imports

--- src/server.js
+++ src/server.js
@@ -1,25 +1,33 @@
-import express from "express";
-import cors from "cors";
-import {Pool, Query} from "pg";
-import dotenv from "dotenv";
+const express = require("express");
+const cors = require("cors");
 
-dotenv.config();
+require("dotenv").config();
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to your Express server" });
 });
+
+// app.get("/api/users", (req,res) => {
+//   try {
+//     const result = "SELECT * FROM users";
+//     res.json(result.rows);
+//   } catch(err) {
+//     console.error("Query error:", err);
+//     res.status(500).json({ error: "Failed to fetch the users!" });
+//   }
+// });
 
 
 
